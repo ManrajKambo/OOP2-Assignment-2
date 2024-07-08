cf6d305f517c0401f4d7da16d3f62450bf0e9e76
Manraj Kambo <Manraj.Kambo@edu.sait.ca>
2024-07-08 00:16:31 -0600

    Uploaded files

---
+++ GroupAssignment2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GroupAssignment2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ GroupAssignment2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GroupAssignment2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
