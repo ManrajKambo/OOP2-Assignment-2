fed4fe2fcc3fa56f6a92270e50b8d6b706d308e5
Manraj Kambo <Manraj.Kambo@edu.sait.ca>
2024-07-09 18:40:57 -0600

    Changes by Dylane
    Co-Authored-By: Dylane Cunningham <155758209+DylaneElyse@users.noreply.github.com>

---
+++ GroupAssignment2/Flights.cs
@@ -0,0 +1,44 @@
+﻿using GroupAssignment2.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupAssignment2
+{
+    class Flights
+    {
+        string _flightNum;
+        string _airline;
+        string _deptAirport;
+        string _arriveAirport;
+        string _dayOfWeek;
+        string _departTime;
+        int _distance;
+        double _price;
+
+        void _ImportFlightData()
+        {
+            List<Flights> flightObjList = new List<Flights>();
+
+            string[] data = Resources.flights.Split(',');
+
+            foreach (string s in data)
+            {
+                string[] field = s.Split(',');
+
+                flightObjList.Add(new Flights {
+                    _flightNum = field[0],
+                    _airline = field[1],
+                    _deptAirport = field[2],
+                    _arriveAirport = field[3],
+                    _dayOfWeek = field[4],
+                    _departTime = field[5],
+                    _distance = Convert.ToInt32(_flightNum),
+                    _price = Convert.ToDouble(_airline)
+                });
+            }
+        }
+    }
+}
